ccd3ad775128dcdda541ba7093e72b9c80948563
Aleksander <asava@sibmail.com>
2023-01-25 18:21:37 +0700

    createSlice

--- src/store/task.js
+++ src/store/task.js
@@ -1,26 +1,12 @@
-import { createAction, createReducer } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 const initialState = [
   {id: 1, title: 'Task 1', completed: false},
   {id: 2, title: 'Task 2', completed: false}
 ]
-const update = createAction('task/updated')
-const remove = createAction('task/removed')
 
-export function taskComplete(id) {
-    return update({ id, completed: true})
-}
 
-export function titleChenge(id) {
-    return update({ id, title: `New title for ${id}`})
-
-}
-
-export function taskDeleted(id) {
-    return remove({ id })
-}
-
-const taskReducer = createReducer(initialState, (builder) => {
-  builder.addCase(update, (state, action) => {
+const taskSlice = createSlice({name: 'task', initialState, reducers: {
+  update(state, action) {
     const elementIndex = state.findIndex(
       (el) => el.id === action.payload.id
       )
@@ -28,10 +14,26 @@ const taskReducer = createReducer(initialState, (builder) => {
         ...state[elementIndex],
         ...action.payload
       }
-  }).addCase(remove, (state, action) => {
-    return state.filter((el) => el.id !== action.payload.id) // '*' Прямая государственная мутация
-  })
-})
+  },
+  remove(state, action) {
+        return state.filter((el) => el.id !== action.payload.id)
+    }
+}})
 
+const { actions, reducer: taskReducer } = taskSlice
+const { update, remove } = actions
+
+export function taskComplete(id) {
+  return update({ id, completed: true})
+}
+
+export function titleChenge(id) {
+  return update({ id, title: `New title for ${id}`})
+
+}
+
+export function taskDeleted(id) {
+  return remove({ id })
+}
 
 export default taskReducer
