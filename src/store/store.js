d0b96a33a0e3d023f20579214522fe9fae44fe82
Aleksander <asava@sibmail.com>
2023-01-25 11:02:40 +0700

    Create Reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,10 @@
 import { createStore } from "redux";
-import taskReduser from "./task";
+import taskReducer from "./task";
 
-const initialState = [
-    {id: 1, title: 'Task 1', completed: false},
-    {id: 2, title: 'Task 2', completed: false}
-  ]
+
 
 function configureStore(params) {
-    return createStore(taskReduser, initialState)
+    return createStore(taskReducer)
 }
 
 export default configureStore
--- src/store/task.js
+++ src/store/task.js
@@ -1,9 +1,10 @@
-import { createAction } from '@reduxjs/toolkit'
-
+import { createAction, createReducer } from '@reduxjs/toolkit'
+const initialState = [
+  {id: 1, title: 'Task 1', completed: false},
+  {id: 2, title: 'Task 2', completed: false}
+]
 const update = createAction('task/updated')
 const remove = createAction('task/removed')
-
-console.log('update', update)
 
 export function taskComplete(id) {
     return update({ id, completed: true})
@@ -18,24 +19,19 @@ export function taskDeleted(id) {
     return remove({ id })
 }
 
-function taskReduser(state = [], action) {
-    switch (action.type) {
-        case update.type: {
-          const newArray = [...state]
-        const elementIndex = newArray.findIndex(
-          (el) => el.id === action.payload.id
-          )
-          newArray[elementIndex] = {...newArray[elementIndex], ...action.payload}
-          return newArray
-        }
-        case remove.type: {
-            return state.filter((el) => el.id !== action.payload.id)
-        }
-        
-      default:
-        return state
-    }
-    
-  }
+const taskReducer = createReducer(initialState, (builder) => {
+  builder.addCase(update, (state, action) => {
+    const elementIndex = state.findIndex(
+      (el) => el.id === action.payload.id
+      )
+      state[elementIndex] = {
+        ...state[elementIndex],
+        ...action.payload
+      }
+  }).addCase(remove, (state, action) => {
+    return state.filter((el) => el.id !== action.payload.id)
+  })
+})
 
-export default taskReduser
+
+export default taskReducer
