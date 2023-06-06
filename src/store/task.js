ed691a0f9ba41913b8a783723ebebc67f874df6b
Aleksander <asava@sibmail.com>
2023-06-06 14:20:17 +0700

    Configure Store + туда сюда

--- src/store/task.js
+++ src/store/task.js
@@ -2,30 +2,32 @@ import { createAction, createSlice } from '@reduxjs/toolkit'
 import todosService from '../services/todos.service'
 const initialState = []
 
-
 const taskSlice = createSlice({
   name: 'task',
   initialState,
   reducers: {
     recived(state, action) {
-     return state = action.payload
+      return (state = action.payload)
+    },
+    completed(state, action) {
+      const elementIndex = state.findIndex((el) => el.id === action.payload.id)
+      state[elementIndex].completed = !state[elementIndex].completed
     },
     update(state, action) {
-    const elementIndex = state.findIndex(
-      (el) => el.id === action.payload.id
-      )
-      state[elementIndex] = {
-        ...state[elementIndex],
-        ...action.payload
-      }
-  },
-  remove(state, action) {
-        return state.filter((el) => el.id !== action.payload.id)
+      const elementIndex = state.findIndex((el) => el.id === action.payload.id)
+      state[elementIndex].title =
+        state[elementIndex].title === `Task ${action.payload.id}`
+          ? `New title for ${action.payload.id}`
+          : `Task ${action.payload.id}`
+    },
+    remove(state, action) {
+      return state.filter((el) => el.id !== action.payload.id)
     }
-}})
+  }
+})
 
 const { actions, reducer: taskReducer } = taskSlice
-const { update, remove, recived } = actions
+const { update, remove, completed, recived } = actions
 
 const taskRequested = createAction('task/requested')
 const taskRequestFailed = createAction('task/requestFailed')
@@ -41,12 +43,11 @@ export const getTasks = () => async (dispatch) => {
 }
 
 export const completeTask = (id) => (dispatch, getState) => {
-  dispatch(update({ id, completed: true}))
+  dispatch(completed({ id }))
 }
 
 export function titleChenge(id) {
-  return update({ id, title: `New title for ${id}`})
-
+  return update({ id })
 }
 
 export function taskDeleted(id) {
@@ -54,4 +55,3 @@ export function taskDeleted(id) {
 }
 
 export default taskReducer
-
