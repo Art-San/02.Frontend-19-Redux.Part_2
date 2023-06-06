c359acc7e83130d1997754b81078184e1f1058bd
Aleksander <asava@sibmail.com>
2023-06-06 17:43:20 +0700

    useDispatch() + туда сюда

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
-import {completeTask, titleChenge, taskDeleted, getTasks } from './store/task'
-import configureStore from './store/store';
-import { Provider, useSelector, useDispatch } from 'react-redux';
-
-
+import { completeTask, titleChenge, taskDeleted, getTasks } from './store/task'
+import configureStore from './store/store'
+import { Provider, useSelector, useDispatch } from 'react-redux'
 
 const store = configureStore()
 const App = () => {
@@ -13,9 +11,8 @@ const App = () => {
 
   useEffect(() => {
     dispatch(getTasks())
-  }, [])
+  }, [dispatch])
 
- 
   const changeTitle = (taskId) => {
     dispatch(titleChenge(taskId))
   }
@@ -26,28 +23,23 @@ const App = () => {
 
   return (
     <>
-    <h1>App</h1>
-    <ul>
-      {state.map((el) => (
-        <li key={el.id}>
-          <p>{el.title}</p>
-          <p> {`Comleted: ${el.completed}`}</p>
-          <button onClick={() => dispatch(completeTask(el.id))}>
-            Completed
-          </button>
-          <button onClick={() => changeTitle(el.id)}>
-            Chsange Title
-          </button>
-          <button onClick={() => deleteTask(el.id)}>
-            Delete
-          </button>
-          <hr/>
-        </li>
-      ))}
-    </ul>
+      <h1>App</h1>
+      <ul>
+        {state.map((el) => (
+          <li key={el.id}>
+            <p>{el.title}</p>
+            <p> {`Comleted: ${el.completed}`}</p>
+            <button onClick={() => dispatch(completeTask(el.id))}>
+              Completed
+            </button>
+            <button onClick={() => changeTitle(el.id)}>Chsange Title</button>
+            <button onClick={() => deleteTask(el.id)}>Delete</button>
+            <hr />
+          </li>
+        ))}
+      </ul>
     </>
   )
-  
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
