e6b576abfcb28c0be131b338db90dcb1e7d0a093
Aleksander <asava@sibmail.com>
2023-06-06 17:07:53 +0700

    React Redux + Provider + туда сюда

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
-import {completeTask, titleChenge, taskDeleted, getTasks } from './store/task'
-import configureStore from './store/store';
-import { Provider } from 'react-redux';
-
-
+import { completeTask, titleChenge, taskDeleted, getTasks } from './store/task'
+import configureStore from './store/store'
+import { Provider } from 'react-redux'
 
 const store = configureStore()
 const App = () => {
@@ -17,7 +15,6 @@ const App = () => {
     })
   }, [])
 
- 
   const changeTitle = (taskId) => {
     store.dispatch(titleChenge(taskId))
   }
@@ -28,34 +25,31 @@ const App = () => {
 
   return (
     <>
-    <h1>App</h1>
-    <ul>
-      {state.map((el) => (
-        <li key={el.id}>
-          <p>{el.title}</p>
-          <p> {`Comleted: ${el.completed}`}</p>
-          <button onClick={() => store.dispatch(completeTask(el.id))}>
-            Completed
-          </button>
-          <button onClick={() => changeTitle(el.id)}>
-            Chsange Title
-          </button>
-          <button onClick={() => deleteTask(el.id)}>
-            Delete
-          </button>
-          <hr/>
-        </li>
-      ))}
-    </ul>
+      <h1>App</h1>
+      <ul>
+        {state.map((el) => (
+          <li key={el.id}>
+            <p>{el.title}</p>
+            <p> {`Comleted: ${el.completed}`}</p>
+            <button onClick={() => store.dispatch(completeTask(el.id))}>
+              Completed
+            </button>
+            <button onClick={() => changeTitle(el.id)}>Chsange Title</button>
+            <button onClick={() => deleteTask(el.id)}>Delete</button>
+            <hr />
+          </li>
+        ))}
+      </ul>
     </>
   )
-  
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider
+      store={store} // React Redux. Providing Store
+    >
       <App />
     </Provider>
   </React.StrictMode>
