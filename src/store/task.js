ddb8e6b473bfcec4180c879b936259559b0ad062
Aleksander <asava@sibmail.com>
2023-06-06 22:28:18 +0700

    селекторы + туда сюда

--- src/store/task.js
+++ src/store/task.js
@@ -1,43 +1,58 @@
 import { createSlice } from '@reduxjs/toolkit'
 import todosService from '../services/todos.service'
 import { setError } from './error'
-const initialState = {entities: [], isLoading: true }
-
+const initialState = { entities: [], isLoading: true }
 
 const taskSlice = createSlice({
-    name: 'task',
-    initialState,
-    reducers: {
-      recived(state, action) {
-        state.entities = action.payload
-        state.isLoading = false
-      },
-      update(state, action) {
+  name: 'task',
+  initialState,
+  reducers: {
+    recived(state, action) {
+      state.entities = action.payload
+      state.isLoading = false
+    },
+    completed(state, action) {
       const elementIndex = state.entities.findIndex(
         (el) => el.id === action.payload.id
-        )
-        state.entities[elementIndex] = {
-          ...state.entities[elementIndex],
-          ...action.payload
-        }
-      },
-      remove(state, action) {
-        state.entities = state.entities.filter(
-            (el) => el.id !== action.payload.id
-        );
-      },
-      taskRequested(state) {
-        state.isLoading = true
-      },
-      taskRequestFailed(state, action) {
-        state.isLoading = false
-      }
+      )
+      state.entities[elementIndex].completed =
+        !state.entities[elementIndex].completed
+    },
+    update(state, action) {
+      const elementIndex = state.entities.findIndex(
+        (el) => el.id === action.payload.id
+      )
+      state.entities[elementIndex].title =
+        state.entities[elementIndex].title === `Task ${action.payload.id}`
+          ? `New title for ${action.payload.id}`
+          : `Task ${action.payload.id}`
+    },
+    // update(state, action) {
+    //   const elementIndex = state.entities.findIndex(
+    //     (el) => el.id === action.payload.id
+    //   )
+    //   state.entities[elementIndex] = {
+    //     ...state.entities[elementIndex],
+    //     ...action.payload
+    //   }
+    // },
+    remove(state, action) {
+      state.entities = state.entities.filter(
+        (el) => el.id !== action.payload.id
+      )
+    },
+    taskRequested(state) {
+      state.isLoading = true
+    },
+    taskRequestFailed(state, action) {
+      state.isLoading = false
     }
+  }
 })
 
 const { actions, reducer: taskReducer } = taskSlice
-const { update, remove, recived, taskRequested, taskRequestFailed } = actions
-
+const { update, remove, recived, completed, taskRequested, taskRequestFailed } =
+  actions
 
 export const loadTasks = () => async (dispatch) => {
   dispatch(taskRequested())
@@ -51,20 +66,18 @@ export const loadTasks = () => async (dispatch) => {
 }
 
 export const completeTask = (id) => (dispatch, getState) => {
-  dispatch(update({ id, completed: true}))
+  dispatch(completed({ id }))
 }
 
 export function titleChenge(id) {
-  return update({ id, title: `New title for ${id}`})
-
+  return update({ id })
 }
 
 export function taskDeleted(id) {
   return remove({ id })
 }
 
-export const getTasks = () => (state) => state.tasks.entities
-export const getTasksLoadingStatus = () => (state) => state.tasks.isLoading
+export const getTasks = () => (state) => state.tasks.entities // Создаём селекторы
+export const getTasksLoadingStatus = () => (state) => state.tasks.isLoading // Создаём селекторы
 
 export default taskReducer
-
