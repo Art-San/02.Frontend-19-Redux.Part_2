b531defc9475844147e47481c2ee9e05aa6f2e62
Aleksander <asava@sibmail.com>
2023-06-06 18:16:12 +0700

    загрузку и обработку ошибок + туда сюда

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
-import {completeTask, titleChenge, taskDeleted, getTasks } from './store/task'
-import configureStore from './store/store';
-import { Provider, useSelector, useDispatch } from 'react-redux';
-
-
+import { completeTask, titleChenge, taskDeleted, getTasks } from './store/task'
+import configureStore from './store/store'
+import { Provider, useSelector, useDispatch } from 'react-redux'
 
 const store = configureStore()
 const App = () => {
   const state = useSelector((state) => state.entities)
-  const isLoading = useSelector(state => state.isLoading)
-  const error = useSelector(state => state.error)
+  const isLoading = useSelector((state) => state.isLoading)
+  const error = useSelector((state) => state.error)
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(getTasks())
-  }, [])
+  }, [dispatch])
 
- 
   const changeTitle = (taskId) => {
     dispatch(titleChenge(taskId))
   }
@@ -34,28 +31,23 @@ const App = () => {
 
   return (
     <>
-    <h1>App</h1>
-    <ul>
-      {state.map((el) => (
-        <li key={el.id}>
-          <p>{el.title}</p>
-          <p> {`Comleted: ${el.completed}`}</p>
-          <button onClick={() => dispatch(completeTask(el.id))}>
-            Completed
-          </button>
-          <button onClick={() => changeTitle(el.id)}>
-            Chsange Title
-          </button>
-          <button onClick={() => deleteTask(el.id)}>
-            Delete
-          </button>
-          <hr/>
-        </li>
-      ))}
-    </ul>
+      <h1>App</h1>
+      <ul>
+        {state.map((el) => (
+          <li key={el.id}>
+            <p>{el.title}</p>
+            <p> {`Comleted: ${el.completed}`}</p>
+            <button onClick={() => dispatch(completeTask(el.id))}>
+              Completed
+            </button>
+            <button onClick={() => changeTitle(el.id)}>Chsange Title</button>
+            <button onClick={() => deleteTask(el.id)}>Delete</button>
+            <hr />
+          </li>
+        ))}
+      </ul>
     </>
   )
-  
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
---
+++ test.js
@@ -0,0 +1,57 @@
+import { createAction, createSlice } from '@reduxjs/toolkit'
+import todosService from '../services/todos.service'
+const initialState = []
+
+const taskSlice = createSlice({
+  name: 'task',
+  initialState,
+  reducers: {
+    recived(state, action) {
+      return (state = action.payload)
+    },
+    completed(state, action) {
+      const elementIndex = state.findIndex((el) => el.id === action.payload.id)
+      state[elementIndex].completed = !state[elementIndex].completed
+    },
+    update(state, action) {
+      const elementIndex = state.findIndex((el) => el.id === action.payload.id)
+      state[elementIndex].title =
+        state[elementIndex].title === `Task ${action.payload.id}`
+          ? `New title for ${action.payload.id}`
+          : `Task ${action.payload.id}`
+    },
+    remove(state, action) {
+      return state.filter((el) => el.id !== action.payload.id)
+    }
+  }
+})
+
+const { actions, reducer: taskReducer } = taskSlice
+const { update, remove, completed, recived } = actions
+
+const taskRequested = createAction('task/requested')
+const taskRequestFailed = createAction('task/requestFailed')
+
+export const getTasks = () => async (dispatch) => {
+  dispatch(taskRequested())
+  try {
+    const data = await todosService.fetch()
+    dispatch(recived(data))
+  } catch (error) {
+    dispatch(taskRequestFailed(error.message))
+  }
+}
+
+export const completeTask = (id) => (dispatch, getState) => {
+  dispatch(completed({ id }))
+}
+
+export function titleChenge(id) {
+  return update({ id })
+}
+
+export function taskDeleted(id) {
+  return remove({ id })
+}
+
+export default taskReducer
